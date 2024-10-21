5562d1f2549fd974e713807ede79078af2673c38
alan <marinalan396@gmail.com>
2024-10-21 11:46:01 -0300

    actualizacion de todos los .py

--- src/main/python/dhs2024/Escucha.py
+++ src/main/python/dhs2024/Escucha.py
@@ -1,8 +1,16 @@
 from antlr4 import ErrorNode, TerminalNode
 from compiladoresListener import compiladoresListener
 from compiladoresParser import compiladoresParser
+from TablaSimbolos import TablaSimbolos
+from Contexto import Contexto
+from ID import ID
 
 class Escucha (compiladoresListener) :
+    tablaDeSimbolos = TablaSimbolos()
+
+    numTokensTotal = 0
+    numNodosTotal = 0
+
     numTokens = 0
     numNodos = 0
 
@@ -13,20 +21,31 @@ class Escucha (compiladoresListener) :
         print("Fin de la compilacion")
 
     def enterIwhile(self, ctx:compiladoresParser.IwhileContext):
-        print("Encontre WHILE")
-        print("\tCantidad hijos: " + str(ctx.getChildCount()))
-        print("\tTokens: " + str(ctx.getText()))
+        print("Encontre WHILE\n")
 
     def exitIwhile(self, ctx:compiladoresParser.IwhileContext):
         print("FIN del WHILE")
         print("\tCantidad hijos: " + str(ctx.getChildCount()))
-        print("\tTokens: " + str(ctx.getText()))
+        print("\tTokens: " + str(ctx.getText())+"\n")
 
     def enterDeclaracion(self, ctx:compiladoresParser.DeclaracionContext):
         print("### Declaracion")
 
     def exitDeclaracion(self, ctx:compiladoresParser.DeclaracionContext):
-        print("Nombre Variable: " + ctx.getChild(1).getText())
+        tipoDeDato = ctx.getChild(0).getText()
+        NombreVariable = ctx.getChild(1).getText()
+
+        if(self.tablaDeSimbolos.buscarGlobal(NombreVariable) != 1):
+            self.tablaDeSimbolos.buscarLocal(NombreVariable)
+
+        self.tablaDeSimbolos.addIdentificador(NombreVariable, tipoDeDato)
+
+    def enterAsignacion(self, ctx: compiladoresParser.AsignacionContext):
+        print("### ASIGNACION ###")
+
+    def exitAsignacion(self, ctx: compiladoresParser.AsignacionContext):
+        print("ya no hay variables que asignar\n")
+
 
     def visitTerminal(self, node: TerminalNode):
         #print("----> Token: " + node.getText())
@@ -37,3 +56,26 @@ class Escucha (compiladoresListener) :
 
     def enterEveryRule(self, ctx):
         self.numNodos += 1
+        self.numNodosTotal += 1
+
+    def enterBloque(self, ctx:compiladoresParser.BloqueContext):
+        print('***Entre a un CONTEXTO***\n')
+        contexto = Contexto()
+        self.tablaDeSimbolos.addContexto(contexto)
+
+
+    def exitBloque(self, ctx: compiladoresParser.BloqueContext):
+        print('***Sali de un CONTEXTO***')
+        print('Cantidad de hijos: ' + str(ctx.getChildCount()))
+        print('TOKENS: ' + ctx.getText())
+
+        print("En este contesto se encontro: ")
+        self.tablaDeSimbolos.contextos[-1].imprimirTabla()
+        print("*" * 20 + "\n")
+        self.tablaDeSimbolos.delContexto()
+
+    def exitPrograma(self, ctx:compiladoresParser.ProgramaContext):
+        print('Fin compilacion\n')
+        print('Se encontraron: \n')
+        print('Nodos: ' + str(self.numNodosTotal) + "\n")
+        print('Tokens: ' + str(self.numTokensTotal) + "\n")
